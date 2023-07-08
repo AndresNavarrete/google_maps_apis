cfe949eb895a328af1c4aa139c8ae46d06423cec
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-08 18:24:35 -0400

    update: test

--- main_test.py
+++ main_test.py
@@ -6,7 +6,10 @@ if __name__ == "__main__":
     api_key = "AIzaSyBpTac3buTxXFoCNT6gMVBwpMWxKiRLOXI"
     api = Routes(api_key)
     r = api.get_route(
-        origin='2da B Juárez 102, Los Jales, Ex Hacienda de Coscotitlán, 42064 Pachuca de Soto, Hgo., Mexico',
-        destination='Calz. de Tlalpan 3465, Sta. Úrsula Coapa, Coyoacán, 04650 Ciudad de México, CDMX, Mexico'
+        origin="2da B Juárez 102, Los Jales, Ex Hacienda de Coscotitlán, 42064 Pachuca de Soto, Hgo., Mexico",
+        destination="Calz. de Tlalpan 3465, Sta. Úrsula Coapa, Coyoacán, 04650 Ciudad de México, CDMX, Mexico",
+        avoidTolls=False,
+        departureTime="Mañana a las 10 am",
+        timezone="America/Mexico_City",
     )
-    print(r.json())
+    print(r)
