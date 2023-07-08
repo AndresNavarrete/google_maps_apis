5e2761319346f8233af81f6f47d055b8e8140b87
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-07 20:02:03 -0400

    update docs

--- packages/clients/google_maps_sdk.py
+++ packages/clients/google_maps_sdk.py
@@ -2,7 +2,7 @@ from googlemaps import Client
 from packages.clients.base_client import BaseClient
 
 
-### Docs https://github.com/googlemaps/google-maps-services-python
+# Docs https://github.com/googlemaps/google-maps-services-python
 class GoogleMapsSDK(BaseClient):
     def __init__(self, api_key):
         super().__init__(api_key)
