fa1f4f4c06e2f4d0e00763d3fae28abe336c1201
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-09 19:44:00 -0400

    update: input parse with trip module

--- packages/input_parser.py
+++ packages/input_parser.py
@@ -2,14 +2,41 @@ import json
 
 import pandas
 
+from packages.models.trip_request import TripRequest
+
 
 class InputParser:
     def __init__(self, input_path):
         self.input_path = input_path
-        self.input_data = None
+        self.requests = list()
+        self.trips = list()
         self.load_input()
 
     def load_input(self):
+        self.read_excel()
+        self.generate_trip_requests()
+
+    def read_excel(self):
         df = pandas.read_excel(self.input_path, sheet_name="Viajes")
         input_json = df.to_json(orient="records")
         self.requests = json.loads(input_json)
+
+    def generate_trip_requests(self):
+        for row in self.requests:
+            new_trip = self.get_trip_requests(row)
+            self.trips.append(new_trip)
+
+    def get_trip_requests(self, row):
+        return TripRequest(
+            id=row["ID"],
+            origen_lat=row["Origen_Lat"],
+            origen_lng=row["Origen_Lng"],
+            origen_address=row["Origen_str"],
+            destination_lat=row["Destino_Lat"],
+            destination_lng=row["Destino_Lng"],
+            destination_adress=row["Destino_str"],
+            modo=row["Modo"],
+            hora=row["Hora"],
+            id_viaje=row["ID_Viaje"],
+            avoid_tolls=row["Parameter_avoid"],
+        )
