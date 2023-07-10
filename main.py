b1ad8958685d6e3b732c7057aeabeccc30f8742b
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-10 00:19:22 -0400

    add:sys argument

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from sys import argv
+
 from packages.app import App
 
 if __name__ == "__main__":
     api_key = "AIzaSyBpTac3buTxXFoCNT6gMVBwpMWxKiRLOXI"
-    app = App(api_key=api_key, input_file="request.xlsx", service_name="")
+    input_file = "request.xlsx"
+    service_name = argv[1]
+    app = App(api_key=api_key, input_file=input_file, service_name=service_name)
     app.execute()
