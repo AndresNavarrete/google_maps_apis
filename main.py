e47c21938342c7a1e0713fd74d6af0c42373fed4
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-11 11:06:01 -0400

    update: main file

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from packages.app import App
 if __name__ == "__main__":
     api_key = "AIzaSyBpTac3buTxXFoCNT6gMVBwpMWxKiRLOXI"
     input_file = "request.xlsx"
-    service_name = argv[1]
-    app = App(api_key=api_key, input_file=input_file, service_name=service_name)
+
+    app = App(api_key=api_key, input_file=input_file, service_name=argv[1])
     app.execute()
