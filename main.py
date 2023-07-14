1e7ff20262221ee3600caeda776c4976d2e92efa
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-13 23:20:50 -0400

    update: default value

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from packages.app import App
 if __name__ == "__main__":
     api_key = "AIzaSyBpTac3buTxXFoCNT6gMVBwpMWxKiRLOXI"
     input_file = "request.xlsx"
-    excel = False
+    excel = True
 
     app = App(api_key=api_key, input_file=input_file, service_name=argv[1], excel=excel)
     app.execute()
